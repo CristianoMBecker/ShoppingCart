d7c2e585af25b67230858545701018e15e3c2dfa
Cristiano <crisbecker50@gmail.com>
2022-10-11 17:19:43 -0300

    fix lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,12 +2,12 @@ const getUrl = (pc) => `https://api.mercadolibre.com/sites/MLB/search?q=${pc}`;
 
 const fetchProducts = async (pc) => {
   if (pc === undefined || pc === '') {
-    throw new Error('You must provide an url')
+    throw new Error('You must provide an url');
   }
   const data = await fetch(getUrl(pc));
   const response = data.json();
   return response;
-}
+};
 
 if (typeof module !== 'undefined') {
   module.exports = {
