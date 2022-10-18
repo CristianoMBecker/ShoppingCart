a5adb9b01aee25017cf10839f509e99aba7e1311
Cristiano <crisbecker50@gmail.com>
2022-10-18 17:45:15 -0300

    requisito 9

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ const createProductImageElement = (imageSource) => {
 
 const cart = document.querySelector('.cart__items');
 
+const somar = (pricePc, prices) => {
+  const price = document.querySelector('.total-price');
+  prices.push(pricePc);
+  const sum = prices.reduce((total, amount) => total + amount);
+  price.innerText = `$${sum.toFixed(2)}`;
+};
+
+const CalculatePrice = async () => {
+  const prices = [];
+  const carrinho = document.querySelectorAll('.insideCart');
+  carrinho.forEach((product) => {
+    const splitted = product.innerText.split('|');
+    const doestnmatter = Object.values(splitted);
+    const pcPrices = doestnmatter[2];
+    const pricePc = parseFloat(pcPrices.replace('PRICE: $', ''));
+    somar(pricePc, prices);
+  });
+};
+
 const saveLocal = () => {
   const allItems = [];
   const savedItems = document.querySelectorAll('.insideCart');
@@ -78,9 +97,10 @@ const getIdFromProductItem = (product) => product.querySelector('span.id').inner
 
 const cartItemClickListener = (li) => {
   cart.removeChild(li);
+  CalculatePrice();
 };
 
-/**
+  /**
  * Função responsável por criar e retornar um item do carrinho.
  * @param {Object} product - Objeto do produto.
  * @param {string} product.id - ID do produto.
@@ -95,6 +115,7 @@ const createCartItemElement = ({ id, title, price }) => {
   li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
   li.addEventListener('click', () => {
     cartItemClickListener(li);
+    saveLocal();
   });
     cart.appendChild(li);
     saveLocal();
@@ -107,6 +128,7 @@ const rescueLocal = () => {
 
   const saved = JSON.parse(getSavedCartItems());
   saved.forEach((savedPC) => createCartItemElement(savedPC));
+  CalculatePrice();
 };
 
 const shoppingCart = async () => {
@@ -119,6 +141,7 @@ const shoppingCart = async () => {
   botoes.forEach((btt, index) => {
     btt.addEventListener('click', () => {
       createCartItemElement(results[index]);
+      CalculatePrice();
     });
   });
 };
@@ -127,6 +150,8 @@ const cleaner = document.querySelector('.empty-cart');
 cleaner.addEventListener('click', () => {
   cart.innerText = '';
   localStorage.clear();
+  const price = document.querySelector('.total-price');
+  price.innerText = '';
 });
 
 window.onload = () => {
